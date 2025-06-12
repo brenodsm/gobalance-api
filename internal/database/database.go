938cae5b3b13050d17e1c1d20b014c93810513ab
Breno dos Santos Moreira <breno.dsmnb@gmail.com>
2025-06-12 13:31:22 -0300

    feat: adicionar função de conexão com banco de dados PostgreSQL

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"database/sql"
+
+	"github.com/brenodsm/gobalance-api/config"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+// OpenConnection estabelece e retorna uma conexão com o banco de dados PostgreSQL.
+func OpenConnection() (*sql.DB, error) {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		return nil, err
+	}
+	db, err := sql.Open("pgx", cfg.ConnectionString)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
+}
